c079a9095060d9482c168a730e7a5c6d6847650f
Muhammd Maaz <161757387+maaz-official@users.noreply.github.com>
2024-09-05 09:28:55 -0700

    UPdated Animation

--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -2,28 +2,32 @@ import React, { useState } from 'react';
 import './Switch.css'; // Import the CSS for styling
 
 const ToggleSwitch = () => {
-  const [isChecked, setIsChecked] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
 
-  const toggleSwitch = () => {
-    setIsChecked(!isChecked);
-  };
-
-  return (
-    <div className="switch-container">
-      <div
-        className={`switch ${isChecked ? 'switch--on' : 'switch--off'}`}
-        onClick={toggleSwitch}
-      >
-        <div className="switch-circle">
-          <div className="icon-wrapper">
-            <span role="img" aria-label="emoji" className={`icon ${isChecked ? 'icon--happy' : 'icon--sad'}`}>
-              {isChecked ? '😄' : '😟'}
-            </span>
+    const toggleSwitch = () => {
+      setIsChecked(!isChecked);
+    };
+  
+    return (
+      <div className="threeD-switch-container">
+        <div
+          className={`threeD-switch ${isChecked ? 'threeD-switch--on' : 'threeD-switch--off'}`}
+          onClick={toggleSwitch}
+        >
+          <div className="threeD-switch-circle">
+            <div className="emoji-wrapper">
+              <span
+                role="img"
+                aria-label="emoji"
+                className={`emoji-ball ${isChecked ? 'emoji-ball--happy' : 'emoji-ball--sad'}`}
+              >
+                {isChecked ? '😄' : '😟'}
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default ToggleSwitch;
